e520c9bf956052468a481206e112affb976f607c
290_100008737535_winter <290_100008737535_winter@git.cloud.tencent.com>
2019-03-15 17:59:42 +0800

    inital GJExtension

---
+++ Resource/Student.h
@@ -0,0 +1,19 @@
+//
+//  Student.h
+//  GCDTest
+//
+//  Created by winterJiao on 2019/3/15.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Student : NSObject
+
+- (void)eat;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Resource/Student.m
@@ -0,0 +1,17 @@
+//
+//  Student.m
+//  GCDTest
+//
+//  Created by winterJiao on 2019/3/15.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+- (void)eat {
+    NSLog(@"吃饭。。。。");
+}
+
+@end
